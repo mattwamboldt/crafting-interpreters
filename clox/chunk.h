53717b8445d07deededf5b36e5aadb25d58dfe85
Matt Wamboldt <mattwamboldt@hotmail.com>
2023-07-23 19:38:17 -0300

    Chatper 14 - Starting clox, Chunk and memory representation

---
+++ clox/chunk.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "common.h"
+#include "value.h"
+
+enum OpCode
+{
+    OP_CONSTANT,
+    OP_RETURN,
+};
+
+struct Chunk
+{
+    int count;
+    int capacity;
+    uint8_t* code;
+    int* lines;
+    ValueArray constants;
+};
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
--- clox/main.cpp
+++ clox/main.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
-using namespace std;
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
-int main()
+/*
+NOTE: This whole thing is in a very C only style. Normally I'd take advantage
+of C++ extensions that make things more readable, like structs having functions.
+
+This code is essentially from the book with few modifications so bare that in mind.
+*/
+
+/*
+* Challenges:
+* - Chapter 14.1: find a way to rle the line numbers so they can be stored more efficiently.
+* - Chapter 14.2: add an OP_CONSTANT_LONG that stores the operand as a larger 24bit num
+* - Chapter 14.3: (sounds fun) implement realloc without library functions, only one large malloc at boot
+*/
+
+int main(int argc, const char* argv[])
 {
-	cout << "Hello World! Press enter" << endl;
-	char key;
-	cin >> key;
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
+    return 0;
 }
