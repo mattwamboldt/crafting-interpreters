544c88a0f64d34913876ac8ccfacfc608912de3e
Matt Wamboldt <mattwamboldt@hotmail.com>
2023-07-26 21:54:14 -0300

    Chapter 16 - Scanner

--- clox/vm.h
+++ clox/vm.h
@@ -22,6 +22,6 @@ enum InterpretResult
 
 void initVM();
 void freeVM();
-InterpretResult interpret(Chunk* chunk);
+InterpretResult interpret(const char* source);
 void push(Value value);
 Value pop();
