ab95af8dd22087842348d17577052b10712f2d94
Matt Wamboldt <mattwamboldt@hotmail.com>
2023-08-11 15:25:53 -0300

    Chapter 27: Classes and Instances

--- clox/object.h
+++ clox/object.h
@@ -2,25 +2,32 @@
 
 #include "common.h"
 #include "chunk.h"
+#include "table.h"
 #include "value.h"
 
 #define OBJ_TYPE(value)     ((value).asObject()->type)
 
+#define IS_CLASS(value)     isObjType(value, OBJ_CLASS)
 #define IS_CLOSURE(value)   isObjType(value, OBJ_CLOSURE)
 #define IS_FUNCTION(value)  isObjType(value, OBJ_FUNCTION)
+#define IS_INSTANCE(value)  isObjType(value, OBJ_INSTANCE)
 #define IS_NATIVE(value)    isObjType(value, OBJ_NATIVE)
 #define IS_STRING(value)    isObjType(value, OBJ_STRING)
 
+#define AS_CLASS(value)     ((ObjClass*)((value).asObject()))
 #define AS_CLOSURE(value)   ((ObjClosure*)((value).asObject()))
 #define AS_FUNCTION(value)  ((ObjFunction*)((value).asObject()))
+#define AS_INSTANCE(value)  ((ObjInstance*)((value).asObject()))
 #define AS_NATIVE(value)    (((ObjNative*)((value).asObject()))->function)
 #define AS_STRING(value)    ((ObjString*)((value).asObject()))
 #define AS_CSTRING(value)   (((ObjString*)((value).asObject()))->chars)
 
 enum ObjType
 {
+    OBJ_CLASS,
     OBJ_CLOSURE,
     OBJ_FUNCTION,
+    OBJ_INSTANCE,
     OBJ_NATIVE,
     OBJ_STRING,
     OBJ_UPVALUE
@@ -74,8 +81,23 @@ struct ObjClosure
     int upvalueCount;
 };
 
+struct ObjClass
+{
+    Obj obj;
+    ObjString* name;
+};
+
+struct ObjInstance
+{
+    Obj obj;
+    ObjClass* klass;
+    Table fields;
+};
+
+ObjClass* newClass(ObjString* name);
 ObjClosure* newClosure(ObjFunction* function);
 ObjFunction* newFunction();
+ObjInstance* newInstance(ObjClass* klass);
 ObjNative* newNative(NativeFn function);
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
